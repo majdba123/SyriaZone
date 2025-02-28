cc2f6f585ef060db40202f1484d3686d68e5348e
majdba123 <137396563+majdba123@users.noreply.github.com>
2025-03-01 00:56:51 +0300

    create
    create vue_project and DB without connection

---
+++ SyriaZone/app/Http/Controllers/SubCategortController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sub_Categort;
+use Illuminate\Http\Request;
+
+class SubCategortController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sub_Categort $sub_Categort)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sub_Categort $sub_Categort)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Sub_Categort $sub_Categort)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sub_Categort $sub_Categort)
+    {
+        //
+    }
+}
